af2ff7b4ccfcbe2577afccf625c9b71b68bf069d
Masood Mansoori <masood.mansoori@gmail.com>
2019-05-02 20:56:02 -0700

    minor changes

--- wget.py
+++ wget.py
@@ -58,7 +58,7 @@ def wget(urldict):
 			command1='cd '+directory_name+" && "+'wget -o /dev/stdout -t 2 --waitretry=1 --referer='+'"'+config.referrer+'"'+" "+'--user-agent='+'"'+useragent+'"'+'--header='+'"'+"Accept-Encoding: gzip"+'" '+url+ ' | tee -a '+debugfile
 		else:
 			command1='cd '+directory_name+" && "+'wget -o /dev/stdout -A '+'"'+config.filetypes+'" '+'-t 2 --waitretry=1 --referer='+'"'+config.referrer+'"'+" "+'--user-agent='+'"'+useragent+'"'+'--header='+'"'+"Accept-Encoding: gzip"+'" '+url+ ' | tee -a '+debugfile
-		print command1		
+		
 		os.system(command1.strip())
 
 
