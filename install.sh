a8a106d90801cd71a24c7f84c58ff626522ea544
Masood Mansoori <masood.mansoori@gmail.com>
2019-05-05 13:59:03 -0700

    minor changes

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Exit immediately if a command exits with a non-zero status
-
-sudo apt-get update
-
-sudo apt-get install -y nano python nano make gcc autoconf automake libtool python-dev libpcre3-dev
-sudo apt-get install clamav
-sudo freshclam
